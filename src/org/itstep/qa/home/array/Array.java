ffb3ee2d60f0e10002a0296e8d41ad49e3a9fac2
mike <mike@gmail.com>
2018-09-27 16:21:33 +0300

    update one

--- src/org/itstep/qa/home/array/Array.java
+++ src/org/itstep/qa/home/array/Array.java
@@ -14,13 +14,24 @@ public class Array {
     */
 
     // получение отсортированного массива
-    public void getSortArray(int [] array){
+    public int[] getSortArray(int [] array){
+        Arrays.sort(array,0,array.length);
+        for(int i = 0; i < array.length; i++){
+            //System.out.println(array[i] + " ");
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
+    }
+
+    // получение отсортированного массива void
+    /*public void getSortArray(int [] array){
         Arrays.sort(array,0,array.length);
         for(int i = 0; i < array.length; i++){
             //System.out.println(array[i] + " ");
     }
         System.out.println(Arrays.toString(array));
-    }
+    }*/
+
     /*public void getSortArray(int [] array){
         Arrays.sort(array,0,array.length);
         System.out.println("Отсортированный массив: ");
---
+++ src/org/itstep/qa/home/array/ArrayOnTen.java
@@ -0,0 +1,16 @@
+package org.itstep.qa.home.array;
+
+import java.util.Arrays;
+
+public class ArrayOnTen {
+    /*Напишите метод, увеличивающий элементы массива на 10%. Определите, какие у этого метода входные и выходные данные.
+     Решите сами, должен ли метод изменять исходный массив, или в результате работы будет создаваться новый.
+      */
+
+    public void getArrayIncreaseOnTen(int [] array){
+        for(int i = 0; i < array.length; i++){
+            //System.out.println(array[i] + " ");
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
